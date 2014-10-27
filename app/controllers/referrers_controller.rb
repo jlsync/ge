0690832dd975d33c9be460fab09213d43d75d57c
James Jenkins-Yates <james@jamesjenkinsyates.com>
2014-10-27 03:26:14 +0100

    further changes to tracking

--- app/controllers/referrers_controller.rb
+++ app/controllers/referrers_controller.rb
@@ -9,12 +9,12 @@ class ReferrersController < ApplicationController
   def redirect
     @business = Business.find(params[:business_id])
     @referrer = @business.referrers.find(params[:id])
-    unless cookies.signed[params[:id]]     # if no cookie for this referrer id
+    unless cookies[params[:id]]     # if no cookie for this referrer id
       @referrer.increment!(:click_count)  # then increment as we haven't seen it before
       # and now set cookie so that we don't record duplicate clicks
-      cookies.signed[params[:id]] = { value: 'r' , path: "/#{@business.id}/" } 
+      cookies[params[:id]] = { value: 'r' , path: "/#{@business.id}/" } 
       # and record the last referrer to credit an conversion to....
-      cookies.signed[:last] = { value: params[:id], path: "/#{@business.id}/" } 
+      cookies[:last] = { value: params[:id], path: "/#{@business.id}/" } 
     end
     if @referrer.business.destination_url =~ /\Ahttp/
       url = @referrer.business.destination_url
@@ -26,12 +26,12 @@ class ReferrersController < ApplicationController
 
   def track
     @business = Business.find(params[:business_id])
-    if rid = cookies.signed[:last] and cookies.signed[rid] == 'r' and @referrer = @business.referrers.find(rid)
+    if rid = cookies[:last] and cookies[rid] == 'r' and @referrer = @business.referrers.find(rid)
       @referrer.increment!(:conversion_count)
       # now record the conversion in the cookie so we dont later double count 
       # (double counting will probably still occur in safari and other 
       # browsers that don't allow 3rd parties to set cookies.
-      cookies.signed[params[:id]] = { value: 'c' , path: "/#{@business.id}/" } 
+      cookies[params[:id]] = { value: 'c' , path: "/#{@business.id}/" } 
     end
     expires_now
     send_file Rails.root.join("public", "track.gif"), type: "image/gif", disposition: "inline", filename: 't.gif'
